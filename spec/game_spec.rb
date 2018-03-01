494a29a709fd279621858e844cf827165222fb80
Ed <ea.litten@gmail.com>
2018-03-01 12:59:11 +0000

    Add tests for getting & swapping players

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,12 @@ require './lib/game'
 
 describe Game do
 
-  let(:player) { double :player }
-  let(:player_class) { double :player_class, new: player }
+  let(:player1) { double :player, name: 'X' }
+  let(:player2) { double :player, name: 'O' }
+  let(:player_class) { double :player_class }
   subject(:game) { described_class.new(player_class) }
+  
+  before(:each) { allow(player_class).to receive(:new).and_return(player1, player2) }
 
   describe '#initialize' do
     it 'should create two players at start' do
@@ -14,6 +17,25 @@ describe Game do
 
     it 'should start with empty 3x3 board by default' do
       expect(game.board).to eq [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+    end
+  end
+
+  describe '#current_player' do
+    it 'should return the first player' do
+      expect(game.current_player).to eq player1
+    end
+  end
+
+  describe '#current_player' do
+    it 'should return the second player' do
+      expect(game.opponent).to eq player2
+    end
+  end
+
+  describe '#swap_players' do
+    it 'should swap current_player and opponent' do
+      game.swap_players
+      expect(game.current_player).to eq player2
     end
   end
 end
