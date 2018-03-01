c6edc39caf00d3856b8c2e49b2ff41ac0b96831a
Ed <ea.litten@gmail.com>
2018-03-01 12:59:47 +0000

    Add fn's on Game to get & swap players

--- lib/game.rb
+++ lib/game.rb
@@ -5,8 +5,20 @@ class Game
   attr_reader :board
 
   def initialize(player_class = Player, starting_board = create_blank_board)
-    @players = [player_class.new, player_class.new]
+    @players = [player_class.new('X'), player_class.new('O')]
     @board = starting_board
+  end
+
+  def current_player
+    @players.first
+  end
+
+  def opponent
+    @players.last
+  end
+
+  def swap_players
+    @players.reverse!
   end
 
   private
